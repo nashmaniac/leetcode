19d866ca7b03e00d767d9559e8511885803e8845
Raju Ahmed Shetu <shetu2153@gmail.com>
2022-02-11 00:19:31 -0500

    updated

---
+++ golang/3_longest_substring_without_duplicate.go
@@ -0,0 +1,31 @@
+package golang
+
+type longestSubstringWithoutRepeatProblem struct {
+}
+
+func (l *longestSubstringWithoutRepeatProblem) Variation1(s string) int {
+	maxLength := 0
+	startIndex := 0
+	var hashmap map[byte]int = make(map[byte]int)
+
+	for i := 0; i < len(s); i++ {
+		if lastIndex, ok := hashmap[s[i]]; ok {
+			startIndex = max(startIndex, lastIndex+1)
+		}
+		currentLength := i - startIndex + 1
+		maxLength = max(currentLength, maxLength)
+		hashmap[s[i]] = i
+	}
+	return maxLength
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func lengthOfLongestSubstring(s string) int {
+	l := longestSubstringWithoutRepeatProblem{}
+	return l.Variation1(s)
+}
